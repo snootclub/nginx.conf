d82dc10040f76de28764d7312613c3f732f82afa
chee <chee@snoot.club>
2018-11-26 11:53:12 +0000

    use arch linux docker image for snoots

--- skeleton/application/.start.sh
+++ skeleton/application/.start.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-apt-get -y update
-apt-get -y install openssh-server
-mkdir -p /var/run/sshd
-/usr/sbin/sshd
+pacman -Syu --noconfirm opensshd nodejs npm pm2
+for key_type in dsa rsa ecdsa ed25519; do
+	ssh-keygen -t dsa -N "" -f /etc/ssh/ssh_host_${key_type}_key
+done
+/bin/sshd
 mkdir -p /root/.ssh
-update-rc.d ssh defaults
 mv /application/authorized_keys /root/.ssh/authorized_keys
 chown -R root.root /root/.ssh
-npm i -g pm2
 cd /application
 pm2 ecosystem.config.js
 pm2 start --name "snoot" --watch npm -- start
