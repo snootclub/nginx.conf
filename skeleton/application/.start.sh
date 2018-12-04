a484292e25363deb42c987dd2f2f691463b4bbd0
chee <chee@snoot.club>
2018-12-04 08:06:47 +0000

    yes everything

--- skeleton/application/.start.sh
+++ skeleton/application/.start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-pacman -Syu --noconfirm openssh nodejs npm pm2 python neovim
-ssh-keygen -A
-/bin/sshd
+apt update
+apt install -y vim-tiny mg openssh-server
+/usr/sbin/sshd
 mkdir -p /root/.ssh
 mv /application/authorized_keys /root/.ssh/authorized_keys
 chown -R root.root /root/.ssh
+chmod 700 -R /root/.ssh
 cd /application
+npm i -g pm2
 pm2 ecosystem.config.js
 pm2 start --name "snoot" --watch npm -- start
 tail -f /dev/null
